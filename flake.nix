49e4f8cd598ac1c232609ca7707b7c8cef0647c8
hiina <hiina@hiina.space>
2026-04-05 23:20:50 -0600

    make home-manager use flakes too

--- flake.nix
+++ flake.nix
@@ -11,12 +11,17 @@
     nixpkgs-xr.url = "github:nix-community/nixpkgs-xr";
     comfyui-nix.url = "github:utensils/comfyui-nix";
 
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
     # https://github.com/mic92/sops-nix
     sops-nix.url = "github:Mic92/sops-nix";
     sops-nix.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, nixpkgs, nixpkgs-xr, comfyui-nix, sops-nix }: {
+  outputs = { self, nixpkgs, nixpkgs-xr, comfyui-nix, home-manager, sops-nix }: {
     nixosConfigurations = {
       sayu = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
@@ -33,6 +38,15 @@
           sops-nix.nixosModules.sops
         ];
       };
+    };
+
+    homeConfigurations."s" = home-manager.lib.homeManagerConfiguration {
+      pkgs = import nixpkgs {
+        system = "x86_64-linux";
+        config.allowUnfree = true;
+        overlays = [ nixpkgs-xr.overlays.default ];
+      };
+      modules = [ ./home.nix ];
     };
   };
 }
