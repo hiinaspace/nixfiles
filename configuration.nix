b43d6e5aa5103102d34680058e0445d9c419004e
hiina <hiina@hiina.space>
2026-06-01 11:38:23 -0600

    add binutils

--- configuration.nix
+++ configuration.nix
@@ -253,7 +253,8 @@
   # List packages installed in system profile.
   # You can use https://search.nixos.org/ to find more packages (and options).
   environment.systemPackages = with pkgs; [
-    neovim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+    binutils
+    neovim
     wget
     grim # screenshot
     slurp # more screenshot
